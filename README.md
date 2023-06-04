06cb28e1ab50b0f313b98a1f696c00f3cedddc3c
Salvador <salvadorpm@gmail.com>
2023-06-03 18:48:00 -0600

    fragments y growls

--- README.md
+++ README.md
@@ -4,10 +4,19 @@ Este es un proyecto creado con spring boot, versión 17 de java y 3.1.0 de sprin
 
 ## Descripción
 
-En este proyecto se crea un crud a la tabla clientes. Se incluyen entidades, dao, services(facade pattern) y controladores. También contiene los templates creados con thymeleaf, el archivo import.sql y los archivos properties para la
-configuración de mensajes.  
-Bd: Mysql
-Utilicé bootstrap 5.3 en el front.
+En este proyecto se crea un crud a la tabla clientes. Se incluyen:
+- Entidades
+- Dao
+- Services(facade pattern) 
+- Controladores. 
+- Templates creados con thymeleaf
+- Archivo import.sql 
+- Archivos properties para la configuración de mensajes.  
+- Bd: Mysql
+- Bootstrap 5.3 en el front.
+- Fragmentos de thymeleaf
+- Alerts con boostrap
+- Growl con librerías toastr y sweetalert
 
 ## Autor
 
--- src/main/java/com/salvador/springboot/app/controladores/ClienteControl.java
+++ src/main/java/com/salvador/springboot/app/controladores/ClienteControl.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.salvador.springboot.app.entidades.Cliente;
 import com.salvador.springboot.app.service.IClienteService;
@@ -36,37 +37,47 @@ public class ClienteControl {
 		Cliente cliente = new Cliente();
 		model.addAttribute("titulo", "Agregar datos del cliente");
 		model.addAttribute("cliente", cliente);
+		model.addAttribute("valueBoton", "Agregar");
 		return "form";
 	}
 
 	@GetMapping("/form/{id}")
-	public String formularioEdicion(@PathVariable(value = "id") Long id, Model model) {
+	public String formularioEdicion(@PathVariable(value = "id") Long id, Model model, RedirectAttributes flash) {
 		Cliente cliente = null;
 		if (id > 0) {
 			cliente = clienteService.findOne(id);
+			if(cliente==null) {
+				flash.addFlashAttribute("error", "El id del cliente no existe en la base de datos");
+				return "redirect:/listaClientes";	
+			}
 		} else {
+			flash.addFlashAttribute("error", "El id del cliente no puede ser cero");
 			return "redirect:/listaClientes";
 		}
 		model.addAttribute("titulo", "Editar cliente");
 		model.addAttribute("cliente", cliente);
+		model.addAttribute("valueBoton", "Editar");
 		return "form";
 	}
 
 	@PostMapping("/form")
-	public String guardarCliente(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+	public String guardarCliente(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
 		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Ingreso de datos de cliente");
 			return "form";
 		}
-		clienteService.save(cliente);
+		String mensaje = cliente.getId() != null ? "Registro editado!" : "Registro guardado";		
+		clienteService.save(cliente);		
 		status.setComplete();
+		flash.addFlashAttribute("success", mensaje);
 		return "redirect:listaClientes";
 	}
 
 	@GetMapping("/eliminar/{id}")
-	public String eliminar(@PathVariable(value = "id") Long id) {
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 		if (id > 0) {
 			clienteService.eliminar(id);
+			flash.addFlashAttribute("info", "Registro borrado correctamente");
 		}
 		return "redirect:/listaClientes";
 	}
